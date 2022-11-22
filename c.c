235fe53742f1c55861c6962dae1d96a1483c3c7e
Joyce-Zo <109672607+Joyce-Zo@users.noreply.github.com>
2022-11-22 23:55:04 +0800

    Add files via upload

--- c.c
+++ c.c
@@ -1,7 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 
-
+//       合并两个有序数组
+// 
 //void merge(int* nums1, int nums1Size,int m, int* nums2, int nums2Size, int n)
 //{
 //    int n1 = 0, n2 = 0;
